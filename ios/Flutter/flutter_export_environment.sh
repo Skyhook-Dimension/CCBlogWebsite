287435815892c2efcf6b72e3f1f22b07a8477ab1
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-08-02 22:20:12 +0530

    just a commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-<<<<<<< HEAD
-export "FLUTTER_APPLICATION_PATH=E:\CCBlogWebsite-unofficial--master\CCBlogWebsite-unofficial-"
-=======
-export "FLUTTER_APPLICATION_PATH=D:\cc blog\CCBlogWebsite-unofficial-"
->>>>>>> upstream/master
+export "FLUTTER_ROOT=C:\src\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Chirag\00_data\flutterfiles\mywebapp"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
